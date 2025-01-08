9ec9f941a8df8da717996a9d65b0870fcf44435b
holysz <szymonholysz@student.agh.edu.pl>
2025-01-08 23:34:59 +0100

    animal test

--- src/test/java/darwin/model/animal/AnimalTest.java
+++ src/test/java/darwin/model/animal/AnimalTest.java
@@ -1,8 +1,41 @@
 package darwin.model.animal;
 
+import darwin.model.Vector2d;
+import darwin.util.AnimalState;
+import darwin.util.Directions;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AnimalTest {
+    Vector2d position = new Vector2d(5, 5);
+    AnimalProperties properties = new AnimalProperties(new int[]{0},
+            0,
+            new HashSet<>(),
+            new HashSet<>(),
+            0,
+            0,
+            Directions.NORTH,
+            2,
+            position,
+            AnimalState.ALIVE);
+
+    @Test
+    void createChildren() {
+        AbstractAnimal mother = new Animal(properties);
+        AbstractAnimal father = new Animal(properties);
+        AbstractAnimal child = mother.createChildren(father, 1, 1,1);
+
+        assertEquals(1, mother.getProperties().getEnergy());
+        assertEquals(1, father.getProperties().getEnergy());
+        assertEquals(2, child.getProperties().getEnergy());
+        assert child.getProperties().getParents().contains(mother);
+        assert child.getProperties().getParents().contains(father);
+        assert mother.getProperties().getChildren().contains(child);
+        assert father.getProperties().getChildren().contains(child);
+        assertEquals(1, child.getProperties().getGenome().length);
+        assertNotEquals(0,child.getProperties().getGenome()[0]);
+    }
 }
