815aebebf6934dfca8b2195d1eab1a5926feea9a
Wpawlina <wpawlina@student.agh.edu.pl>
2025-01-14 14:26:29 +0100

    fixed animalFactory and added testing of movement on the map

--- src/main/java/darwin/model/animal/AnimalFactory.java
+++ src/main/java/darwin/model/animal/AnimalFactory.java
@@ -8,10 +8,20 @@ import java.util.HashSet;
 
 public class AnimalFactory {
     public static AbstractAnimal createAnimal(Boolean crazy, int genomeLength,int initEnergy, Vector2d position){
-        AnimalProperties properties = new AnimalProperties(new int[genomeLength], 0,new HashSet<AbstractAnimal>(), new HashSet<AbstractAnimal>(), 0, 0, Directions.NORTH, initEnergy, position, AnimalState.ALIVE);
+        AnimalProperties properties = new AnimalProperties(AnimalFactory.randomGenome(genomeLength), 0,new HashSet<AbstractAnimal>(), new HashSet<AbstractAnimal>(), 0, 0, Directions.NORTH, initEnergy, position, AnimalState.ALIVE);
         if(crazy)
             return new AnimalCrazy(properties);
         else
             return new Animal(properties);
     }
+
+    private static int[] randomGenome(int genomeLength){
+        int[] genome = new int[genomeLength];
+        for(int i = 0; i < genomeLength; i++){
+            genome[i] = (int)(Math.random() * 8);
+        }
+        return genome;
+    }
+
+
 }
